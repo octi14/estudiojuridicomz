5bcb96c89f0041ab819f2b57993a92367e29f786
octi14 <gonzalezoctavio5@gmail.com>
2026-04-10 02:53:05 -0300

    favicon

--- app.vue
+++ app.vue
@@ -3,3 +3,12 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+import { useHead } from '@unhead/vue'
+import favicon from '~/assets/img/icono-balanza.svg'
+
+useHead({
+  link: [{ rel: 'icon', type: 'image/svg+xml', href: favicon }]
+})
+</script>
